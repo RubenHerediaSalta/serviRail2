1cbbfecb7f712b72c755be86bd4b8aab04b7aa0e
unknown <ruben.a.her@gmail.com>
2023-12-08 15:32:34 -0300

    jwtk

--- controllers/AuthControllers.js
+++ controllers/AuthControllers.js
@@ -8,45 +8,14 @@ const generatePassword = async (password) => {
   return await hash(password, salt);
 };
 
+const JWT_KEY = "fiverrsupersecretkey";
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (email, userId) => {
   // @ts-ignore
-  return jwt.sign({ email, userId }, process.env.JWT_KEY, {
+  return jwt.sign({ email, userId }, JWT_KEY, {
     expiresIn: maxAge,
   });
 };
-
-//REVISAR APROX 2H VIDEO
-// export const signup = async (req, res, next) => {
-//   try {
-//     const prisma = new PrismaClient();
-//     const { email, password } = req.body;
-//     if (email && password) {
-//       const user = await prisma.user.create({
-//         data: {
-//           email,
-//           password: await generatePassword(password),
-//         },
-//       });
-//       return res.status(201).json({
-//         user: { id: user?.id, email: user?.email },
-//         jwt: createToken(email, user.id),
-//       });
-//     } else {
-//       return res.status(400).send("Email and Password Required");
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     if (err instanceof Prisma.PrismaClientKnownRequestError) {
-//       if (err.code === "P2002") {
-//         return res.status(400).send("Email Already Registered");
-//       }
-//     } else {
-//       return res.status(500).send("Internal Server Error");
-//     }
-//     throw err;
-//   }
-// };
 
 export const signup = async (req, res, next) => {
   try {
